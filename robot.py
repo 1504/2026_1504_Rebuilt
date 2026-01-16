2d3d708b2c6f843f9c1ee93f0a977056fa32452b
Diego-Weiss <diego@chrisweiss.com>
2026-01-16 13:58:29 -0500

    added more imports for robot.py

--- robot.py
+++ robot.py
@@ -1,6 +1,20 @@
 import wpilib
 import wpimath
+import navx
+import drivesubsystem
+import commands2
+import climb
+import intake
+import constants
 import wpilib.drive
 import wpimath.filter
 import wpimath.controller
-import navx
+
+from wpilib import Timer
+
+# To see messages from networktables, you must setup logging
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+class MyRobot(commands2.TimedCommandRobot):

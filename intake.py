d2ab4aa3ae2c907edcfd5e591cc66c25c7a8b854
Diego-Weiss <diego@chrisweiss.com>
2026-01-18 18:07:02 -0500

    added some stuff for intake

--- intake.py
+++ intake.py
@@ -6,3 +6,15 @@ from commands2 import Subsystem, Command
 from rev import SparkMax, SparkMaxConfig, SparkBase
 import math
 import constants
+
+class IntakeSubsystem(Subsystem):
+    def __init__(self):
+        super().__init__()
+
+        self.leftMotor = rev.SparkMax(12, rev.SparkMax.MotorType.kBrushless)
+        self.rightMotor = rev.SparkMax(11, rev.SparkMax.MotorType.kBrushless)
+
+        self.fuelsensor = DigitalInput(9)
+        #self.fuel2 = DigitalInput(8)
+        self.intake_complete = False
+        self.timer = wpilib.Timer()

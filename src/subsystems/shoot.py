6eb9e094d885cbee9e39f0b3410feb7ffca36672
ZapZap131 <ikedayeeter@gmail.com>
2026-02-04 18:17:15 -0500

    WE WILL WIN

--- src/subsystems/shoot.py
+++ src/subsystems/shoot.py
@@ -1,2 +1,20 @@
 import wpilib
 import constants
+class ShootSubsystem(Subsystem):
+    def __init__(self):
+        super().__init__()
+
+        .#0 & 1 are placeholder numbers
+        self.leftMotor = rev.SparkMax(12, rev.SparkMax.MotorType.kBrushless)
+        self.rightMotor = rev.SparkMax(11, rev.SparkMax.MotorType.kBrushless)
+        .#self.placeholderNumber = 0 #Replace with actual number
+    
+        self.shoot_complete = False
+        self.timer = wpilib.Timer()
+
+        .#conveyorDetector.whileActiveContinuous(new RunMotor());
+        .#self.motors = wpilib.MotorControllerGroup(self.leftMotor, self.rightMotor)
+def shoot(self)
+    .#button press and go
+        self.leftMotor.set(-0.2)
+        self.rightMotor.set(0.2)
